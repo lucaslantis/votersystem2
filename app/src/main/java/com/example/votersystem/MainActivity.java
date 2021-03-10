e1d2ab933ebb1bd40d98b345d2aac932714af431
quanTran <lucaslantis0411@gmail.com>
2021-03-09 16:09:34 -0800

    trial test commit 1

--- app/src/main/java/com/example/votersystem/MainActivity.java
+++ app/src/main/java/com/example/votersystem/MainActivity.java
@@ -10,4 +10,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }//trial test
+    //trial test 2
 }
